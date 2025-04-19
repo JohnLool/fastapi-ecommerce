463ac0fec12574213e52ed7fc110a1e525898aa8
JohnLool <pinaev-misha-pinaev@yandex.ru>
2025-04-19 09:32:58 +0500

    base repo, config, db, logger, authx config added

---
+++ app/core/authx_security.py
@@ -0,0 +1,9 @@
+from authx import AuthX, AuthXConfig
+from app.core.config import settings
+
+
+config = AuthXConfig()
+config.JWT_SECRET_KEY = settings.SECRET_KEY
+config.JWT_ALGORITHM = settings.ALGORITHM
+
+security = AuthX(config=config)
---
+++ app/core/config.py
@@ -0,0 +1,23 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class Settings(BaseSettings):
+    DB_HOST: str
+    DB_PORT: int
+    DB_USER: str
+    DB_PASS: str
+    DB_NAME: str
+
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    REFRESH_TOKEN_EXPIRE_DAYS: int
+    SECRET_KEY: str
+
+    @property
+    def database_url(self):
+        return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASS}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
+
+    model_config = SettingsConfigDict(env_file=".env")
+
+
+settings = Settings()
